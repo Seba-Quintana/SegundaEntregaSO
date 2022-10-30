6cfd0cb2b522e42bdeab3e7311d753940d7b6bdb
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-10-30 15:20:02 -0300

    agrego cerrado de procesos

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,32 +8,64 @@ namespace SistemaOperativo
 	{
 		public static void Main(string[] args)
 		{
+			/// convierto de direccion abosluta a relativa
+			
+			string pathA = @"C:/Users/seba8/Desktop/SO/LiebreTortuga/LiebreTortuga";
+			string pathB = @"C:/LiebreTortuga/LiebreTortuga";
+
+			System.Uri uriA = new Uri(pathA);
+			System.Uri uriB = new Uri(pathB);
+			Uri relativeUri = uriA.MakeRelativeUri(uriB);
+			string relativeToA = relativeUri.ToString();
+
 			// hacer varios y que se esperen para ejecutarse (Listo) solo puede haber uno en ejecucion
 
 			Process proceso1 = new Process();
 
-			proceso1.StartInfo.FileName = "cmd.exe";
-			proceso1.StartInfo.Arguments = @"/K dotnet run --project C:/Users/seba8/Desktop/SO/LiebreTortuga/LiebreTortuga";//cd C:\\Users\\seba8\\Desktop\\SO\\SegundaEntrega\\Program\\proceso1.cs; dotnet run
+			proceso1.StartInfo.FileName = $"cmd.exe";
+			proceso1.StartInfo.Arguments = @$"/K dotnet run --project  {relativeToA} && exit";
+
+			/*Process proceso1 = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					CreateNoWindow = false,
+					UseShellExecute = true,
+					FileName = "C:/Users/seba8/Desktop/SO/LiebreTortuga/LiebreTortuga/Program.cs",
+					WindowStyle = ProcessWindowStyle.Normal,
+					//Arguments = "dotnet run --project {relativeToA}"
+				}
+			};*/
 
 			//proceso1.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
-
-
-			//proceso1.Start();
 
 			Process proceso2 = new Process();
 
-			proceso2.StartInfo.FileName = "cmd.exe";
-			proceso2.StartInfo.Arguments = @"/K dotnet run --project C:/Users/seba8/Desktop/SO/LiebreTortuga/LiebreTortuga";//cd C:\\Users\\seba8\\Desktop\\SO\\SegundaEntrega\\Program\\proceso1.cs; dotnet run
+			proceso2.StartInfo.FileName = $"cmd.exe";
+			proceso2.StartInfo.Arguments = @$"/K dotnet run --project  {relativeToA} && exit";
 
 			//proceso1.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
 
+			proceso1.Start();
+			
+			proceso1.WaitForExit();
+			proceso1.CloseMainWindow();
+			proceso1.Close();
 
-			//proceso2.Start();
+
+			proceso2.Start();
+
+			proceso2.WaitForExit();
+
+			proceso2.CloseMainWindow();
+			proceso2.Close();
+			
+
 
 			Process proceso3 = new Process();
 
 			proceso3.StartInfo.FileName = "cmd.exe";
-			proceso3.StartInfo.Arguments = @"/K dotnet run --project C:/Users/seba8/Desktop/SO/LiebreTortuga/LiebreTortuga";//cd C:\\Users\\seba8\\Desktop\\SO\\SegundaEntrega\\Program\\proceso1.cs; dotnet run
+			proceso3.StartInfo.Arguments = @$"/K dotnet run --project  {relativeToA}";
 
 			//proceso1.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
 
@@ -43,7 +75,7 @@ namespace SistemaOperativo
 			Process proceso4 = new Process();
 
 			proceso4.StartInfo.FileName = "cmd.exe";
-			proceso4.StartInfo.Arguments = @"/K dotnet run --project C:/Users/seba8/Desktop/SO/LiebreTortuga/LiebreTortuga";//cd C:\\Users\\seba8\\Desktop\\SO\\SegundaEntrega\\Program\\proceso1.cs; dotnet run
+			proceso4.StartInfo.Arguments = @$"/K dotnet run --project  {relativeToA}";
 
 			//proceso1.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
 
@@ -53,10 +85,9 @@ namespace SistemaOperativo
 			Process proceso5 = new Process();
 
 			proceso5.StartInfo.FileName = "cmd.exe";
-			proceso5.StartInfo.Arguments = @"/K dotnet run --project C:/Users/seba8/Desktop/SO/LiebreTortuga/LiebreTortuga";//cd C:\\Users\\seba8\\Desktop\\SO\\SegundaEntrega\\Program\\proceso1.cs; dotnet run
+			proceso5.StartInfo.Arguments = $@"/K dotnet run --project  {relativeToA}";
 
 			//proceso1.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
-
 
 			//proceso5.Start();
 
